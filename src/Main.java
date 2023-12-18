941672945ed17959562844f5c5295b05a5987b18
621352 <camarenburt>
2023-12-18 09:32:30 -0500

    wqfs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        boolean done = false;
+        double total = 0;
+        do{
+            total += InputHelper.getRangedDouble(scan, "Please enter the item price: $", 0.50, 9.99);
+            if (!InputHelper.getYNConfirm(scan, "Do you want to add another item? [Y/N]")){
+                done = true;
+            }
+        } while (!done);
+        System.out.printf("Total Cost in $: %8.2f", total);
     }
+
 }
